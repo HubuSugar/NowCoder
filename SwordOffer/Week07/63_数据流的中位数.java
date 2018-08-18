20026e7f3111f3563514ac56d5a949d1209024f3
HubuSugar <42160750+HubuSugar@users.noreply.github.com>
2018-08-18 14:29:51 +0800

    Create 63_数据流的中位数.java
    63.如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。我们使用Insert()方法读取数据流，使用GetMedian()方法获取当前读取数据的中位数。

---
+++ SwordOffer/Week07/63_数据流的中位数.java
@@ -0,0 +1,53 @@
+package com.hubu.swordOffer.week07;
+
+import java.util.*;
+
+public class Main_数据流的中位数 {
+
+    //用两个堆，一个大顶堆，一个小顶堆来筛选元素
+
+      private  int count = 0;
+      private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+      private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(15,new Comparator<Integer>(){
+          @Override
+          public int compare(Integer o1, Integer o2) {
+              return o2.compareTo(o1);
+          }
+      });
+
+    public void Insert(Integer num){
+        if (count % 2 == 0){
+            //当数据总数为偶数时,新加入的元素进入小根堆（注意不是直接进入小根堆，而是经过大根堆筛选后的最大元素进入小根堆）
+            //1.元素进入大根堆,
+            maxHeap.offer(num);
+            int filterMaxNum = maxHeap.poll();
+            //从大根堆选出来的最大的元素进入小根堆
+            minHeap.offer(filterMaxNum);
+        }else {
+            //反之，当数据总数为奇数时，元素进入大根堆（同样也是先进入小根堆，经过小根堆筛选的最小元素）
+            minHeap.offer(num);
+            int filterMinNum =  minHeap.poll();
+            maxHeap.offer(filterMinNum);
+        }
+        count++;
+    }
+
+    public Double GetMedian(){
+        if(count % 2 == 0){
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }else{
+           return minHeap.peek() / 1.0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        S63 ss = new S63();
+        int[] a = {49,38,65,97,76,13,27,49,55,04};
+        for (int i = 0; i <  a.length; i++) {
+            ss.Insert(a[i]);
+            Double res = ss.GetMedian();
+            System.out.println("插入数字"+a[i]+"后，中位数为:"+res);
+        }
+    }
+}
